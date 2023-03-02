b9cfcbdc0159bee0faca839a7ac90a3cf3e85671
amans199 <ahmed.ouda1997@gmail.com>
2023-03-02 20:36:09 +0100

    testing

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const http = require("http");
-const port = process.env.PORT || 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("<h1>Hello World</h1>");
-});
-server.listen(port, () => {
-  console.log(`Server running at port ` + port);
-});
+// const http = require("http");
+// const port = process.env.PORT || 3000;
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/html");
+//   res.end("<h1>Hello World</h1>");
+// });
+// server.listen(port, () => {
+//   console.log(`Server running at port ` + port);
+// });
+console.log("hello");
